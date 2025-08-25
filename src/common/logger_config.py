a82cd9750acdb7949de12875e945784fb4515d5e
AlexanderBeli <alexandrbelitskiy@gmail.com>
2025-08-25 13:52:19 +0200

    update: Rich for logger added

--- src/common/logger_config.py
+++ src/common/logger_config.py
@@ -3,6 +3,8 @@
 
 import logging
 
+from rich.logging import RichHandler
+
 from src.common.config.settings import settings  # Import settings for log level
 
 
@@ -11,14 +13,35 @@ def setup_logging() -> None:
     log_level_str = settings.LOG_LEVEL.upper()
     log_level = getattr(logging, log_level_str, logging.INFO)
 
-    logging.basicConfig(
-        level=log_level,
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        handlers=[
-            logging.StreamHandler()  # Output logs to console
-            # logging.FileHandler("app.log") # Optionally, add file output
+    # logging.basicConfig(
+    #     level=log_level,
+    #     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    #     handlers=[
+    #         logging.StreamHandler()  # Output logs to console
+    #         # logging.FileHandler("app.log") # Optionally, add file output
+    #     ],
+    # )
+
+    # Rich settings
+    root_logger = logging.getLogger()
+    root_logger.setLevel(log_level)
+
+    rich_handler = RichHandler(
+        show_time=True,
+        show_level=True,
+        show_path=False,
+        markup=True,
+        tracebacks_word_wrap=True,
+        tracebacks_suppress=[
+            logging,
         ],
     )
+
+    root_logger.addHandler(rich_handler)
+
+    if len(root_logger.handlers) > 1:
+        root_logger.handlers = [rich_handler]
+
     # Suppress verbose logging from libraries
     logging.getLogger("mysql.connector").setLevel(logging.WARNING)
     logging.getLogger("requests").setLevel(logging.WARNING)
