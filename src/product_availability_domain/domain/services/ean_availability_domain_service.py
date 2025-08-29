577daf25f69e667090910adb8f2eb7b0c16bdd13
AlexanderBeli <alexandrbelitskiy@gmail.com>
2025-08-29 12:49:39 +0200

    delete: files not used

--- src/product_availability_domain/domain/services/ean_availability_domain_service.py
+++
@@ -1,4 +0,0 @@
-"""Domain services for complex Product Availability business logic."""
-
-# Add complex business rules here if they involve multiple EANAvailability entities
-# or other domain objects.
